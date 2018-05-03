60547bd61abebf9e63dc330766cf2edf479786d3
Jmiquilena <gregoriomiqui@gmail.com>
2018-05-03 15:44:43 -0400

    Encabezados y el objeto response

--- hello.js
+++ hello.js
@@ -1,12 +1,12 @@
-var http=require("http"),
-    fs = require("fs");
-
+var http= require("http"),
+        fs= require ("fs");
 
 
 
 http.createServer(function(req,res){
     fs.readFile("./index.html",function(err,html){
-        res.write(html);
+        res.writeHead(200,{"Content-Type":"application/json"})
+        res.write(JSON.stringify({nombre:"Jose",Username:"Jmiquilena"}));
         res.end();
     });
 }).listen(3000);
